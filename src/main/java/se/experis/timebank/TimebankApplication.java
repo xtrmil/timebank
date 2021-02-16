3e443a4b481b7a32db2832130ab303f3ceab90c2
Emil Oja <emil.oja@gmail.com>
2021-02-16 09:36:16 +0100

    chore: initial commit

---
+++ src/main/java/se/experis/timebank/TimebankApplication.java
@@ -0,0 +1,13 @@
+package se.experis.timebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimebankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimebankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/experis/timebank/TimebankApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.timebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimebankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
