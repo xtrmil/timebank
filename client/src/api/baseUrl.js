5965863312922dca70e438d48e91d51b9e3f57da
Johnny Hoang <johnnyhoaang@hotmail.com>
2021-03-24 15:38:56 +0100

    initial commit

--- client/src/api/baseUrl.js
+++ client/src/api/baseUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL:"http://localhost:8080/api/v1/"
+    baseURL:"https://timebank-2021-backend.herokuapp.com/api/v1/"
 });
 
 export default Api;
