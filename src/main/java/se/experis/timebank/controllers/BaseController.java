3d50a4dc73d001775fc47b287b1c5a8af008ebd2
Johnny Hoang <johnnyhoaang@hotmail.com>
2021-03-24 17:22:26 +0100

    test deploy

---
+++ src/main/java/se/experis/timebank/controllers/BaseController.java
@@ -0,0 +1,13 @@
+package se.experis.timebank.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    public String singlePageApp (){
+        return "index.html";
+    }
+}
