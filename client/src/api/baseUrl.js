b4615dc39d6457f79bcb3fef83f938ce814012a5
Johnny Hoang <johnnyhoaang@hotmail.com>
2021-03-26 16:59:44 +0100

    flaakan deploy: ready to deploy

--- client/src/api/baseUrl.js
+++ client/src/api/baseUrl.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL:"http://localhost:8080/api/v1/"
-    // baseURL:"https://timebank-2021.herokuapp.com/api/v1/"
+    //baseURL:"http://localhost:8080/api/v1/"
+     baseURL:"https://timebank-2021.herokuapp.com/api/v1/"
 });
 
 export default Api;
